15af86d0f0087652a24fcd0c5bbc5b86c38d71e9
pearyman <liyananhappy@sina.cn>
2021-03-22 15:34:14 +0800

    feat: 测试配置修改

--- 10/ava-demo/src/components/HelloWorld.vue
+++ 10/ava-demo/src/components/HelloWorld.vue
@@ -4,11 +4,6 @@
 
 <script>
 import { autoChart } from "@antv/chart-advisor";
-// data:[
-//   {field1: "2021-01", field2: 100 },
-//   {field1: "2021-02", field2: 300 },
-//   {field1: "2021-03", field2: 500 },
-// ]
 // data: [
 //         { field1: "apple", field2: 100 },
 //         { field1: "banana", field2: 300 },
@@ -16,19 +11,53 @@ import { autoChart } from "@antv/chart-advisor";
 //       ],
 export default {
   name: "HelloWorld",
-  data: function () {
+  data() {
     return {
       data: [
-        { field1: "2021-01", field2: 100 },
-        { field1: "2021-02", field2: 300 },
-        { field1: "2021-03", field2: 800 },
-      ],
+        { name: "London", 月份: "Jan.", 月均降雨量: 18.9 },
+        { name: "London", 月份: "Feb.", 月均降雨量: 28.8 },
+        { name: "London", 月份: "Mar.", 月均降雨量: 39.3 },
+        { name: "London", 月份: "Apr.", 月均降雨量: 81.4 },
+        { name: "London", 月份: "May", 月均降雨量: 47 },
+        { name: "London", 月份: "Jun.", 月均降雨量: 20.3 },
+        { name: "London", 月份: "Jul.", 月均降雨量: 24 },
+        { name: "London", 月份: "Aug.", 月均降雨量: 35.6 },
+        { name: "Berlin", 月份: "Jan.", 月均降雨量: 12.4 },
+        { name: "Berlin", 月份: "Feb.", 月均降雨量: 23.2 },
+        { name: "Berlin", 月份: "Mar.", 月均降雨量: 34.5 },
+        { name: "Berlin", 月份: "Apr.", 月均降雨量: 99.7 },
+        { name: "Berlin", 月份: "May", 月均降雨量: 52.6 },
+        { name: "Berlin", 月份: "Jun.", 月均降雨量: 35.5 },
+        { name: "Berlin", 月份: "Jul.", 月均降雨量: 37.4 },
+        { name: "Berlin", 月份: "Aug.", 月均降雨量: 42.4 }
+      ]
     };
   },
   mounted() {
     const container = document.getElementById("container");
-    autoChart(container, this.data, { toolbar: true, development: true });
-  },
+    autoChart(container, this.data, {
+      toolbar: true,
+      development: true,
+      title: "我们来测试一个图表例子",
+      description: "是的，这是一个名副其实的副标题",
+      config: {
+        configs: {
+          xAxis: { visible: true },
+          columnWidthRatio: 1,
+          tooltip: { visible: true },
+          xField: "月份",
+          yField: "月均降雨量",
+          colorField: "name",
+          seriesField: "name",
+          isGroup: true,
+          autoFit: true,
+          legend: false,
+          label: { visible: true }
+        },
+        type: "Column"
+      }
+    });
+  }
 };
 </script>
 
